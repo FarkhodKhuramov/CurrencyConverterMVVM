b6d3373ad24b99e64ed61306ba21fc743fb315fa
farkhod <f.khuramov@spitamen.com>
2021-09-28 16:33:46 +0500

    Conversion calculation and display result

--- app/src/main/java/com/example/currencyconverter/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/example/currencyconverter/viewmodel/MainActivityViewModel.java
@@ -1,6 +1,7 @@
 package com.example.currencyconverter.viewmodel;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.example.currencyconverter.data.CurrencyWrapper;
 import com.example.currencyconverter.data.Valute;
@@ -8,6 +9,8 @@ import com.example.currencyconverter.repository.CurrencyRepository;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,7 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 public class MainActivityViewModel extends AndroidViewModel {
+    private static final String TAG = "MainActivityViewModel";
     LiveData<CurrencyWrapper> mCurrencyWrapperLiveData;
     CurrencyRepository currencyRepository;
     public MainActivityViewModel(@NonNull Application application) {
@@ -35,11 +39,22 @@ public class MainActivityViewModel extends AndroidViewModel {
     }
 
     public Float convert(@NotNull Float sumRub, @NotNull String ccy){
+        Log.d(TAG, "convert() called with: sumRub = [" + sumRub + "], ccy = [" + ccy + "]");
         Map<String, Valute> currMap = mCurrencyWrapperLiveData.getValue().getValute();
         if (!currMap.containsKey(ccy) || sumRub == null)
             return null;
         Valute valute = currMap.get(ccy);
         return sumRub / valute.getValue() * valute.getNominal();
 
+    }
+
+    public List<String> getCurrencyList(){
+        if (mCurrencyWrapperLiveData.getValue() == null)
+            return null;
+        List<String> resultList = new ArrayList<>();
+        for (Map.Entry<String, Valute> entry : mCurrencyWrapperLiveData.getValue().getValute().entrySet()){
+            resultList.add(entry.getKey() + " - " + entry.getValue().getName());
+        }
+        return resultList;
     }
 }
